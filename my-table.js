42e0b0003681f1d1f5e3c7a43d66c201c8313fac
John Bellizzi <jabellizzi406@gmail.com>
2017-08-15 14:19:04 -0400

    added initial properties

--- my-table.js
+++ my-table.js
@@ -1,12 +1,23 @@
 export default window.define([], function(){
+
+  // ========== Initial Properties ==========
+  var initialProps = {
+    backgroundColor: 'lightblue'
+  }
+
   
   // ========== Paint ==========
-  var paintFunc = function($element){
+  var paintFunc = function($element, layout){
     // Paint goes here..
+
+    /* Use the backgroundColor property in layout to paint background */
+    $element[0].style.backgroundColor = layout.backgroundColor;
   }
+
 
   // ========== Return Properties ==========
   return {
+    initialProperties: initialProps,
     paint: paintFunc
   }
 })
